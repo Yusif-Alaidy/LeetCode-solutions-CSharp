17466341d0790de67cfb5d2623c3a6aec3ed68ca
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-05-21 09:01:28 +0300

    Build: solution for 1974 problem

---
+++ 1974. Minimum Time to Type Word Using Special Typewriter/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _1974._Minimum_Time_to_Type_Word_Using_Special_Typewriter
+{
+    internal class Program
+    {
+
+        static int MinTimeToType(string word)
+        {
+            int res = 0;
+            char cur = 'a';
+
+            foreach (char c in word)
+            {
+                int dist = Math.Abs(c - cur);
+                res += Math.Min(dist, 26 - dist) + 1; // movement time + typing
+                cur = c;
+            }
+
+            return res;
+            
+        }
+        static void Main(string[] args)
+        {
+            int x = MinTimeToType("abc");
+            Console.WriteLine(x);
+         
+            
+        }
+    }
+}
