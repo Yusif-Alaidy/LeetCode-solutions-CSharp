ee36d0016179c57a4806336d77f2a4120b9af5cb
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-05-27 00:30:52 +0300

    Build: Create solution for 1961. Check If String Is a Prefix of Array prbloem

---
+++ 1961. Check If String Is a Prefix of Array/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _1961._Check_If_String_Is_a_Prefix_of_Array
+{
+    public class Solution
+    {
+        public bool IsPrefixString(string s, string[] words)
+        {
+            int currentIndex = 0;
+
+            foreach (string word in words)
+            {
+                if (currentIndex + word.Length > s.Length)
+                    return false;
+
+                if (s.Substring(currentIndex, word.Length) != word)
+                    return false;
+
+                currentIndex += word.Length;
+
+                if (currentIndex == s.Length)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new();            
+
+            Console.WriteLine(s.IsPrefixString("a", ["aa", "aaaa", "banana"]));
+        }
+    }
+}
