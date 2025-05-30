0bc53a826a20ff773815d9cf6470bf943b1a15d0
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-05-30 16:40:42 +0300

    Build: creat solution for leetcode 1967 problem

---
+++ 1967. Numb of string that appear as substrings in word/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1967._Numb_of_string_that_appear_as_substrings_in_word
+{
+    public static class Solution
+    {
+        public static int NumOfStrings(string[] patterns, string word)
+        {
+            int res = 0;
+            for (int i=0; i<patterns.Length; i++)
+            {
+                if (word.Contains(patterns[i]))
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.NumOfStrings(["a", "abc", "bc", "d"], "abc") == 3);
+            Console.WriteLine(Solution.NumOfStrings(["a", "b", "c"], "aaaaabbbbb") == 2);
+        }
+    }
+}
