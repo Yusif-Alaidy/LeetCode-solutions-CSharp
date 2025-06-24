94cda6a976b2e226e7a758eec709d818438a0dd2
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-06-24 16:44:22 +0300

    Build: Create solution for problem number 344

---
+++ 344. Reverse String/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _344._Reverse_String
+{
+    public class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                char temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            char[] input = { 'h', 'e', 'l', 'l', 'o' };
+            solution.ReverseString(input);
+
+            Console.WriteLine(new string(input)); // olleh
+        }
+    }
+}
